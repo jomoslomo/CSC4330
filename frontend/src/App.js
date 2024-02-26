298af2959e9f7f12ff264409d1d8905f37d86c07
mechols1 <mechol8@lsu.edu>
2024-02-26 16:19:48 -0600

    Added checking for if user if logged it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import BuildWizard from './pages/BuildWizard/BuildWizard';
 import Header from './components/Header/Header'; // If you have a Header component
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import UserDashboard from './pages/UserDashboard/UserDashboard';
+import MessagesPage from './pages/MessagesPage/MessagesPage'; // Import the MessagesPage component
+import ConversationsPage from './pages/MessagesPage/ConversationsPage'; // Import the ConversationsPage component
+// Inside your Router and Routes component:
+//import LogoutPage from './pages/LogoutPage/Logout'; 
+
+const AuthWrapper = () => {
+  const location = useLocation(); // current location
+
+  const userLogged = JSON.parse(localStorage.getItem("userLogged"));
+
+  return userLogged
+    ? <Outlet />
+    : (
+      <Navigate
+        to="/"
+        replace
+        state={{ from: location }} // <-- pass location in route state
+      />
+    );
+};
+
+
 function App() {
   return (
     <Router>
@@ -18,8 +41,6 @@ function App() {
         <Route path="/login" element={<LoginPage />} /> {/* Add the login route */}
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/messages" element={<MessagesPage />} /> 
-        <Route path="/conversations/:userId" element={<ConversationsPage />} />
 
         {/* Add more routes as needed */}
       </Routes>
